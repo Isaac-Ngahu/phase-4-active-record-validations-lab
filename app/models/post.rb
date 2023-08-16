6dab3491d5d321d7a2cf1006659bbe04b0aba73a
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-08-16 14:49:24 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title,presence: true
+    validates :content,length: {minimum:250}
+    validates :summary,length: {maximum:250}
+    validates :category,inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_title
+
+
+    def clickbait_title
+        if title.present?
+            clickbait_words = ["Won't Believe","Secret","Guess","Top"]
+            unless clickbait_words.any?{|word| title.include?(word)}
+                errors.add(:title,"Title not clickbait-y")
+            end
+        else
+            "title not present"
+        end
+        
+    end
 end
